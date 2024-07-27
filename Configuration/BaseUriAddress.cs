47823dac33b9d4e2e6285b58927cadbbf53e26ee
Jnstew05 <69932530+Jnstew05@users.noreply.github.com>
2024-07-27 17:31:47 +0100

    inital

---
+++ Configuration/BaseUriAddress.cs
@@ -0,0 +1,9 @@
+﻿namespace JaydenAppUI.Configuration;
+
+public static class BaseUriAddress
+{
+    /*    public static string BaseUrl { get; set; } = "https://localhost:7172";
+    */
+    public static string BaseUrl { get; set; } = "https://jaydenpersonalprojectapi.onrender.com";
+
+}
---
+++ Services/ChatHubService.cs
@@ -0,0 +1,50 @@
+﻿using Blazored.LocalStorage;
+using JaydenAppModels.Models;
+using JaydenAppUI.Configuration;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace JaydenAppUI.Services
+{
+    public class ChatHubService(NavigationManager navigationManager, ILocalStorageService localStorageService)
+    {
+        public NavigationManager NavigationManager = navigationManager;
+        public ILocalStorageService LocalStorageService = localStorageService;
+
+        public EventHandler<GlobalChatMessages>? ReceiveGlobalMessage;
+        private HubConnection? _hubConnection;
+
+        private async Task<string?> GetBearerToken()
+        {
+            return await localStorageService.GetItemAsStringAsync("token");
+        }
+        public async Task InitiateConnection()
+        {
+            //Fix
+            var token = await GetBearerToken();
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl($"{BaseUriAddress.BaseUrl}/ChatHub")
+                .Build();
+            
+            _hubConnection.On<GlobalChatMessages>("ReceiveGlobalMessage", (chatLog) =>
+
+            {
+                ReceiveGlobalMessage!.Invoke(this, (chatLog));
+            });
+
+            await _hubConnection.StartAsync();
+        }
+        public async Task SendGlobalMessage(AuthenticatedUser user, string message)
+        {
+            await _hubConnection!.SendAsync("SendGlobalMessage", user, message);
+        }
+
+        public async Task TerminateConnection()
+        {
+            await _hubConnection!.StopAsync();
+        }
+        
+    } 
+}
+
