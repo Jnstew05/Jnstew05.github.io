4e9808a951db1aff8eee8b05fba9ea6a7778f61a
Jnstew05 <69932530+Jnstew05@users.noreply.github.com>
2024-07-27 17:37:34 +0100

    sfdfdsf

--- Configuration/BaseUriAddress.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JaydenAppUI.Configuration;
-
-public static class BaseUriAddress
-{
-    /*    public static string BaseUrl { get; set; } = "https://localhost:7172";
-    */
-    public static string BaseUrl { get; set; } = "https://jaydenpersonalprojectapi.onrender.com";
-
-}
